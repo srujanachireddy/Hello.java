bcb0c38c764d6eae083bf30c164bed4910896a03
srujanachireddy <chireddysrujanareddy06@gmail.com>
2024-08-26 16:49:31 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello {
     public static void main( String []args ) {
         System.out.println( "Hello World!" );
+        System.out.println("learning to commit changes in git");
     }
 }
