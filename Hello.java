8647d64cb91a8ce7a9479c3f55f300c68c1e4ac8
srujanachireddy <chireddysrujanareddy06@gmail.com>
2024-08-29 12:15:53 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello {
     public static void main( String []args ) {
         System.out.println( "Hello World!" );
         System.out.println("learning to commit changes in git");
-        System.out.println("checking if the commit gets automically updated or not");
+        System.out.println("checking if the commit gets automically updated or not")
     }
 }
